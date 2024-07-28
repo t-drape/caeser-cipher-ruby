29ae33cd24a8aecf4c88f0a309bc7e9d15e22213
t-drape <thebatreview1@gmail.com>
2024-07-28 11:40:44 -0700

    Adhere to RuboCop standards

--- cipher.rb
+++ cipher.rb
@@ -1,34 +1,31 @@
+# frozen_string_literal: true
+
 def cipher(string, num)
   ascii = string.bytes
   arr = []
-  for code in ascii
-    if (code > 64 && code < 91)
-      if ((code + num) > 90)
+  ascii.each do |code|
+    if code > 64 && code < 91
+      if (code + num) > 90
         code = 64 + ((code + num) - 90)
-        arr << code
       else
         code += num
-        arr << code
       end
-    elsif (code > 96 && code < 123)
-      if ((code+num) > 122)
-        code = 96 + ((code+num) - 122)        
-        arr << code
+    elsif code > 96 && code < 123
+      if (code + num) > 122
+        code = 96 + ((code + num) - 122)
       else
         code += num
-        arr << code
       end
-    else
-      arr << code
     end
-  end  
-  x = arr.map {|code| code.chr}
-  x.join("")
+    arr << code
+  end
+  x = arr.map(&:chr)
+  x.join
 end
 
-puts "Message: "
+puts 'Message: '
 string = gets.chomp
-puts "Number: "
+puts 'Number: '
 num = gets.chomp
 num = num.to_i
 
