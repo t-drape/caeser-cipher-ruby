e76ca55ce6f52f0f038a8c101efe1d4481f407f2
t-drape <thebatreview1@gmail.com>
2024-11-02 11:46:11 -0700

    Finish first test

--- cipher.rb
+++ cipher.rb
@@ -1,32 +1,44 @@
 # frozen_string_literal: true
 
-def cipher(string, num)
-  ascii = string.bytes
-  arr = []
-  ascii.each do |code|
-    if code > 64 && code < 91
-      if (code + num) > 90
-        code = 64 + ((code + num) - 90)
-      else
-        code += num
-      end
-    elsif code > 96 && code < 123
-      if (code + num) > 122
-        code = 96 + ((code + num) - 122)
-      else
-        code += num
-      end
-    end
-    arr << code
+class Cipher
+  def initialize
+    @quote = nil
+    @num = nil
   end
-  x = arr.map(&:chr)
-  x.join
+
+  def set_string
+    puts 'Message: '
+    @quote = gets.chomp
+  end
 end
 
-puts 'Message: '
-string = gets.chomp
-puts 'Number: '
-num = gets.chomp
-num = num.to_i
+# def cipher(string, num)
+#   ascii = string.bytes
+#   arr = []
+#   ascii.each do |code|
+#     if code > 64 && code < 91
+#       if (code + num) > 90
+#         code = 64 + ((code + num) - 90)
+#       else
+#         code += num
+#       end
+#     elsif code > 96 && code < 123
+#       if (code + num) > 122
+#         code = 96 + ((code + num) - 122)
+#       else
+#         code += num
+#       end
+#     end
+#     arr << code
+#   end
+#   x = arr.map(&:chr)
+#   x.join
+# end
 
-puts cipher(string, num)
+# puts 'Message: '
+# string = gets.chomp
+# puts 'Number: '
+# num = gets.chomp
+# num = num.to_i
+
+# puts cipher(string, num)
